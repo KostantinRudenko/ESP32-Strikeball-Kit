a387b8ae4449d594e17ba7dcf5c396dd9062bb03
KostantinRudenko <rudenkokostya20@gmail.com>
2025-11-03 08:55:43 +0200

    replaced code with the one which control audio speakers. replace some useless variables and added the useful ones. fixed logic as for led strip

--- ledstrip_esp/ledstrip_esp.ino
+++ ledstrip_esp/ledstrip_esp.ino
@@ -1,35 +1,86 @@
-#pragma region ______________________________ Includes
+
+
+
+#pragma region ________________________________ Constants
+
+const String Gc_sGamepadMAC = "B0:A7:32:2A:BA:10";                // MAC адрес игрового пульта в формате строки "12:34:56:78:9A:BC"
+
+const uint16_t Gc_u16BusyCheckDelayMS = 400;                      // Задержка опроса состояния пина Busy плейера
+
+const uint8_t LED_STRIP_PIN       = 15;                           // управляющий пин светодиодной ленты
+const uint8_t SIREN_PIN           = 17;                           // управляющий пин сирены(идет на реле)
+
+const uint8_t NTF_SEND_WIFI       = 0b00000001;
+const uint8_t NTF_RECV_WIFI       = 0b00000010;
+const uint8_t NTF_SEND_OK_WIFI    = 0b00000100;
+const uint8_t NTF_SEND_FAIL_WIFI  = 0b00001000;
+
+#pragma endregion Constants
+
+
+#pragma region ________________________________ Includes
 
 #include <WiFi.h>
 #include <esp_now.h>
 #include "esp_log.h"
 #include "esp_task_wdt.h"
-#include "FastLED.h"
-#include "global.h"
 #include "macs.h"
-#include "functions.h"
 
 #pragma endregion Includes
-#pragma region ______________________________ Variables
 
-String G_sThisDeviceMAC;
-uint8_t G_aru8ThisDeviceMAC[6];
-uint8_t G_aru8GamepadMAC[6];
 
-uint8_t G_u8MainState = ST_INIT;
-uint8_t G_u8WiFiState = ST_WIFI_INIT;
+#pragma region ________________________________ Variables
+
+enum task_main_states_t {
+  ST_INIT = 0,
+  ST_WAIT_CMD,
+  ST_SEND_ACK
+};
+
+enum task_wifi_states_t {
+  ST_WIFI_INIT=0,
+  ST_WIFI_RUN,
+  ST_WIFI_ERROR
+};
+
+enum Commands {
+  Ping = 1,                 // сканирование
+  PlayTrack                 // проиграть трек
+};
+
+enum base_states_t {
+  PEER_NO_CONNECT = 0,      // точка не подключена
+  PLAYER_NO_INIT,           // плейер не инициализирован
+  PLAYER_BUSY,              // играет трек
+  PLAYER_READY,             // готов
+  UNKNOUWN_CMD              // неизвестная команда
+};
+
+typedef struct msg_esp_now_t {
+  uint8_t cmd;
+  uint8_t data[2]; 
+} msg_esp_now_t;
+
+String G_sThisDeviceMAC;                                          // MAC адрес этого устройства в формате строки "12:34:56:78:9A:BC"
+uint8_t G_aru8ThisDeviceMAC[6];                                   // MAC адрес этого устройства в формате массива байт
+uint8_t G_aru8GamepadMAC[6];                                      // MAC адрес игрового пульта в формате массива байт
+
+esp_now_peer_info_t peerInfo;
+QueueHandle_t queue_in;                                           // очередь входящих сообщений
+
+uint8_t G_u8MainState = ST_INIT;                                  // Состояние главной задачи
+uint8_t G_u8WiFiState = 0;                                        // Состояние задачи WiFi
+
+msg_esp_now_t outMsg;
 
 static TaskHandle_t hTaskWiFi;
 static TaskHandle_t hTaskMain;
 
-esp_now_peer_info_t peerInfo;
-
-QueueHandle_t queue_in;
-msg_esp_now_t outMsg;
-
 #pragma endregion Variables
 
-#pragma region ______________________________ Main
+
+#pragma region ________________________________ Main_task
+
 
 bool sendESP_NOW(bool* res, msg_esp_now_t* msg) {
     //----------------------------------------------------------------------------+
@@ -55,7 +106,7 @@ bool sendESP_NOW(bool* res, msg_esp_now_t* msg) {
             else if (rv & NTF_SEND_FAIL_WIFI) {
                 log_e("Error sending the data");
                 *res = false;
-            }
+            }        
             st = 0;
             return true;
         }   // pdFALSE - если тайм-аут
@@ -63,25 +114,6 @@ bool sendESP_NOW(bool* res, msg_esp_now_t* msg) {
     return false;
 }
 
-void useLed(uint8_t command, msg_esp_now_t* params) {
-    /*
-     * data[0] -> COLOR
-     * data[1] -> TEAM NUMBER
-     * */
-    switch (command) {
-        case CMD_FillStrip:
-            LightLeds(LEDS_AMOUNT, params->data[0]);
-            break;
-
-        case CMD_FillStripByProgress:
-            FillStripByProgress(params->data[0], params->data[1]);
-            break;
-
-        case CMD_ClearStrip:
-            ClearStrip();
-            break;
-    }
-}
 
 bool parseMessage(msg_esp_now_t* rec_msg, msg_esp_now_t* ack_msg) {
     /*
@@ -94,12 +126,17 @@ bool parseMessage(msg_esp_now_t* rec_msg, msg_esp_now_t* ack_msg) {
     if (rec_msg->cmd == Ping) {
         ack_msg->cmd = rec_msg->cmd;
         ack_msg->data[0] = rec_msg->data[0];
-        return true;
+        ack_msg->data[1] = 3;//G_xAudioConnected ? digitalRead(PLAYER_BUSY_PIN) + PLAYER_BUSY : PLAYER_NO_INIT;
+                             //тут надо заменить на что либо разумное
+        log_i("Состояние = %d", ack_msg->data[1]);
+        return true;    
     }
 
-    useLed(rec_msg->cmd, rec_msg);
-    return false;
+    // playTrack(rec_msg->data[1], true);
+    // тут надо вызывать ф-ю работы с лентой
+    return false; 
 }
+
 
 void TaskMain(void *pvParameters) {
     uint32_t rv;
@@ -111,16 +148,16 @@ void TaskMain(void *pvParameters) {
         switch (G_u8MainState) {
             case ST_INIT:           // ждем уведомления о подключении WiFi и готовности ESP-NOW
                 rv = ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-                if (rv) {
+                if (rv)
                     G_u8MainState++;
-                }
                 break;
 
             case ST_WAIT_CMD:       // ожидание сообщения, парсинг
                 // Читаем данные из очереди. Значение 0 в 3-м параметре означает, что не ждем, если очередь пуста.
+                // s = xQueueReceive(queue_in, &qitem, 0);
                 s = xQueueReceive(queue_in, &qitem, portMAX_DELAY);
                 // если данные из очереди получены
-                if (s == pdPASS) {
+                if (s == pdPASS) { 
                     // выполнение команды, подготовка ответного сообщения
                     if (parseMessage(&qitem, &outMsg))
                         G_u8MainState++;
@@ -135,9 +172,13 @@ void TaskMain(void *pvParameters) {
     }
 }
 
-#pragma endregion Main
 
-#pragma region ______________________________ WiFi
+void loop() {}
+
+#pragma endregion Main_task
+
+
+#pragma region ________________________________ WiFi_task
 
 void OnDataSent(const wifi_tx_info_t *info, esp_now_send_status_t status) {
     if (status == ESP_NOW_SEND_SUCCESS)
@@ -146,29 +187,32 @@ void OnDataSent(const wifi_tx_info_t *info, esp_now_send_status_t status) {
         xTaskNotify(hTaskMain, NTF_SEND_FAIL_WIFI, eSetBits);
 }
 
-void OnDataRecv(const esp_now_recv_info_t *info, const uint8_t *data, int len) {
+
+void OnDataRecv(const esp_now_recv_info_t *info, const uint8_t *incomingData, int len) {
     msg_esp_now_t qitem;
 
-    memcpy(&qitem, data, sizeof(msg_esp_now_t));
-    if (xQueueSendToBack(queue_in, &qitem, 0) != pdPASS)
-        log_e("Input queue overflow");
+    memcpy(&qitem, incomingData, sizeof(msg_esp_now_t));
+    // помещаем элемент в конец очереди и не ждем, если очередь переполнена
+    if (xQueueSendToBack(queue_in, &qitem, 0) != pdPASS) 
+        log_e("Input queue overflow");  
+    
 }
+
 
 bool connectToWiFi() {
     WiFi.mode(WIFI_STA);
-
+    
     if (esp_now_init() != ESP_OK) {
         log_i("Error in esp_now_init");
         return false;
     }
-
 
     if (esp_now_register_send_cb(OnDataSent) != ESP_OK) {
         log_e("Error in esp_now_register_send_cb");
         return false;
     }
 
-    peerInfo.channel = 1;
+    peerInfo.channel = 1;  
     peerInfo.encrypt = false;
     memcpy(peerInfo.peer_addr, G_aru8GamepadMAC, 6);
 
@@ -184,31 +228,35 @@ bool connectToWiFi() {
     return true;
 }
 
-void TaskWiFi(void* pvParameters) {
+
+void TaskWiFi(void *pvParameters) {
     uint32_t rv;
     BaseType_t rc;
-
+  
     disableCore0WDT();
 
     for (;;) {
-        switch (G_u8WiFiState) {
-            case ST_WIFI_INIT:
+        switch (G_u8WiFiState) { 
+            case ST_WIFI_INIT:                      // Подключение...
                 if (connectToWiFi()) {
                     xTaskNotifyGive(hTaskMain);
                     G_u8WiFiState = ST_WIFI_RUN;
-
                 }
-                else {
-
+                else
                     G_u8WiFiState = ST_WIFI_ERROR;
-                  }
                 break;
 
-            case ST_WIFI_RUN:
-                if (xTaskNotifyWait(0, NTF_SEND_WIFI, &rv, 0) == pdTRUE) {
+            case ST_WIFI_RUN:                          // Работа
+                // ждем событие передать сообщение или отключить WIFI
+                // Значение слова события возвращается в rv, которое затем проверяется и в зависимости от
+                // установленного бита(ов), мы выполняем команду.
+                // может установить задержку в portMAX_DELAY ?????????????????????????????????????????????????????
+                
+                // rc = xTaskNotifyWait(0, NTF_SEND_WIFI, &rv, portMAX_DELAY);
+                // rc = xTaskNotifyWait(0, NTF_SEND_WIFI, &rv, 0);
+                if (xTaskNotifyWait(0, NTF_SEND_WIFI, &rv, 0) == pdTRUE)
                     esp_now_send(G_aru8GamepadMAC, (uint8_t *) &outMsg, sizeof(msg_esp_now_t));
-                    break;
-                }
+                break;
 
             case ST_WIFI_ERROR:
                 break;
@@ -216,33 +264,37 @@ void TaskWiFi(void* pvParameters) {
     }
 }
 
-#pragma endregion WiFi
+#pragma endregion WiFi_task
 
-void setup() {
-    log_i("setup");
+
+#pragma region ________________________________ Setup_function
+
+void setup() { 
+    // Serial.begin(115200);
+    log_i("Base start.");
+    
     esp_task_wdt_deinit();
 
-    pinMode(LEDSTRIP_PIN, OUTPUT);
-
     G_sThisDeviceMAC = WiFi.macAddress();
-
-    InitLedStrip();
-    FastLED.addLeds<LED_TYPE, LEDSTRIP_PIN, COLOR_ORDER>(leds, LEDS_AMOUNT);
-
     MacStringToByteArray(G_sThisDeviceMAC, G_aru8ThisDeviceMAC);
     MacStringToByteArray(Gc_sGamepadMAC, G_aru8GamepadMAC);
+
+    // Length (with one extra character for the null terminator)
+    int8_t str_len = G_sThisDeviceMAC.length() + 1; 
+    // Prepare the character array (the buffer) 
+    char char_array[str_len];
+    // Copy it over 
+    G_sThisDeviceMAC.toCharArray(char_array, str_len);
+    log_i("G_sThisDeviceMAC = %s", char_array);
+
 
     queue_in = xQueueCreate(10, sizeof(msg_esp_now_t));
     assert(queue_in);
     if (queue_in != NULL) {
-
-        StripAnimation();
-        ClearStrip();
-
         xTaskCreatePinnedToCore(TaskMain, "TaskMain", 10000, NULL, 1, &hTaskMain, 1);
         xTaskCreatePinnedToCore(TaskWiFi, "TaskWiFi", 20000, NULL, 2, &hTaskWiFi, 0);
-    }
-    log_i("led strip started");
+    }  
 }
 
-void loop() {}
+#pragma endregion Setup_function
+
